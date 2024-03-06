b36cfcd7df66372b6c618ab9fd68b0a6236890e1
MR JHON <151718020+MR-JHONz@users.noreply.github.com>
2024-03-06 15:44:26 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,6 @@
+# Encrypted by K-fuscator
+# Github- https://github.com/KasRoudra/k-fuscator
+
+z="
+";jz='/UPG';iz='I UP';nz=' Bot';Iz='DATE';lz='pull';Gz='RIKS';Sz='e ht';Zz='z/Bo';oz='Sanz';Cz='echo';Dz=' " S';kz='RADE';gz=' "SE';Xz='/MR-';Wz='.com';Az='clea';pz='.sh';Vz='thub';Qz='git ';ez='d +x';az='tSan';Oz='otSa';fz=' *';dz='chmo';cz='cd B';Nz='rf B';Fz='G ME';Hz='A UP';Lz='cd';Kz='"';Rz='clon';Pz='nz';Uz='//gi';Yz='JHON';Bz='r';Tz='tps:';Mz='rm -';Ez='EDAN';mz='bash';Jz='....';hz='LESA';bz='z';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$z$Lz$z$Mz$Nz$Oz$Pz$z$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$z$cz$Oz$Pz$z$dz$ez$fz$z$Cz$z$Cz$gz$hz$iz$Iz$jz$kz$Kz$z$Qz$lz$z$mz$nz$oz$pz"
